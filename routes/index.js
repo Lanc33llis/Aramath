ed141ee0682ae88c75acbf72a0d2f99aae99bae3
Lance Ellis <scdelance@gmail.com>
2021-08-01 02:19:27 -0500

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", function (req, res, next) {
+  const options = {
+    page: {
+      title: "Express",
+      css: "index.css",
+    },
+  }
+  res.render("index", options)
+})
 
-module.exports = router;
+module.exports = router
